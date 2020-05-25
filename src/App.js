d01903e2ca2d4b9841fc2e633221eac9e1daefed
SteveImmanuel <steveandreasimanuel@gmail.com>
2020-05-25 23:15:58 +0700

    Create multiple route path

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Login } from './pages/Login';
+import { Main } from './pages/Main';
+import { NotFound } from './pages/NotFound';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { ProtectedRoute, AuthRoute } from './utils/custom-route';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <ProtectedRoute exact path='/' component={Main} />
+        <AuthRoute path='/login' component={Login} />
+        <Route path='*' component={NotFound} />
+      </Switch>
+    </Router>
   );
 }
 
