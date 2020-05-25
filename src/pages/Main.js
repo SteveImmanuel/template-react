470bcacffaa6f59946d01ab228045c4a9a291415
SteveImmanuel <steveandreasimanuel@gmail.com>
2020-05-25 22:41:47 +0700

    Add dummy pages

---
+++ src/pages/Main.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Main = (props) => {
+    return (
+        <div>
+            <h1>This is Main Content Page</h1>
+            <p>This path is protected. Must be authenticated</p>
+        </div>
+    )
+}
