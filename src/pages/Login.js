9c2e346f75e5cbbe265e97c1d78821cd6dd02918
SteveImmanuel <steveandreasimanuel@gmail.com>
2020-05-25 23:15:22 +0700

    Add login and logout button

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import auth from '../utils/auth';
 
 export const Login = (props) => {
-    return (
-        <h1>This is Login Page</h1>
-    )
+  return (
+    <div>
+      <h1>This is Login Page</h1>
+      <button
+        onClick={() => {
+          auth.login(() => {
+            props.history.push('/');
+          });
+        }}>
+        Login
+      </button>
+    </div>
+  )
 }
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import auth from '../utils/auth';
 
 export const Main = (props) => {
-    return (
-        <div>
-            <h1>This is Main Content Page</h1>
-            <p>This path is protected. Must be authenticated</p>
-        </div>
-    )
+  return (
+    <div>
+      <h1>This is Main Content Page</h1>
+      <p>This path is protected. Must be authenticated.</p>
+      <button
+        onClick={() => {
+          auth.logout(() => {
+            props.history.push('/login');
+          });
+        }}>
+        Logout
+      </button>
+    </div>
+  )
 }
